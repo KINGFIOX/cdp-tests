766edee2477057cc8b6565114ff9f37c2a64a41b
wangfiox <wangfiox@gmail.com>
2024-07-16 19:44:08 +0800

    使用多端口的en

--- vsrc/ram.v
+++ vsrc/ram.v
@@ -44,7 +44,7 @@ module DRAM #(
 ) (
     input clk,
     input [ADDR_BITS - 1:0] a,
-    input we,
+    input [3:0] we,
     input [31:0] d,
     output [31:0] spo
 );
@@ -61,6 +61,7 @@ module DRAM #(
         mem[j] = 0;
       end
     end
+
     mem_file = $fopen(`STRINGIFY(`PATH), "r");
     if (mem_file == 0) begin
       $display("[ERROR] Open file %s failed, please check whether file exists!\n", `STRINGIFY(
@@ -70,6 +71,7 @@ module DRAM #(
     $display("[INFO] Data RAM initialized with %s", `STRINGIFY(`PATH));
     $fread(mem_rd, mem_file);
     for (i = 0; i < 2 ** 20; i = i + 2 ** (20 / 2)) begin
+
       for (j = i; j < i + 2 ** (20 / 2); j = j + 1) begin
         mem[j] = {{mem_rd[j][07:00]}, {mem_rd[j][15:08]}, {mem_rd[j][23:16]}, {mem_rd[j][31:24]}};
       end
@@ -79,7 +81,10 @@ module DRAM #(
   assign spo = mem[a];
 
   always @(posedge clk) begin
-    if (we) mem[a] <= d;
+    if (we[0]) mem[a][7:0] <= d[7:0];
+    if (we[1]) mem[a][15:8] <= d[15:8];
+    if (we[2]) mem[a][23:16] <= d[23:16];
+    if (we[3]) mem[a][31:24] <= d[31:24];
   end
 
 endmodule
