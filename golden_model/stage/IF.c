28e2d7bdbca06e85438372228a9663adff1850e7
wangfiox <wangfiox@gmail.com>
2024-07-17 13:52:30 +0800

    写注释, 学习

--- golden_model/stage/IF.c
+++ golden_model/stage/IF.c
@@ -1,16 +1,19 @@
 #include <cpu.h>
 #include <stdio.h>
+
 extern riscv32_CPU_state cpu;
+
 extern uint32_t memory[];
 
-IF2ID IF(uint32_t npc) {
+IF2ID IF(uint32_t npc)
+{
   IF2ID ret;
   Assert(npc < MEM_SZ, "PC out of boundary!\n");
-  ret.inst = memory[npc >> 2];
-  ret.pc = npc;
+  ret.inst = memory[npc >> 2]; // u32 寻址的
+  ret.pc = npc;                // :NOTICE: 这里的时序是: 取指令完成以后, 才会 pc <- npc
   cpu.pc = npc;
   Log("\n=====");
   Log("Fetched instruction 0x%8.8x at PC=0x%8.8x", ret.inst, ret.pc);
-  cpu.npc = cpu.pc + 4;
+  cpu.npc = cpu.pc + 4; // 当然我估计后面会改掉这个值(branch, jal, jalr), 默认是 npc = pc + 4
   return ret;
 }
