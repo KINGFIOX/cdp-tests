aecf62b926daf39c0b6e9bd87c217e1c6cd41479
wangfiox <wangfiox@gmail.com>
2024-07-17 14:51:48 +0800

    写注释, 学习 MEM, WB

--- golden_model/stage/MEM.c
+++ golden_model/stage/MEM.c
@@ -1,20 +1,38 @@
 #include <cpu.h>
 #include <stdint.h>
-extern riscv32_CPU_state cpu;
-extern uint32_t memory[];
-extern peripheral_descr peripherals[];
-bool is_peripheral(uint32_t addr, size_t *id);
 
-uint32_t mem_load(uint32_t addr, AccessMode mode, uint32_t is_signed) {
+/* ---------- ---------- 前向声明 ---------- ---------- */
+
+extern riscv32_CPU_state cpu;
+
+extern uint32_t memory[];
+
+extern peripheral_descr peripherals[];
+
+extern bool is_peripheral(uint32_t addr, size_t *id); // 这个会在
+
+/* ---------- ---------- ---------- ---------- */
+
+/**
+ * @brief load 以及 mmio
+ *
+ * @param addr
+ * @param mode
+ * @param is_signed
+ * @return uint32_t
+ */
+uint32_t mem_load(uint32_t addr, AccessMode mode, uint32_t is_signed)
+{
   Log("Load");
   Log("Mem addr = %8.8x", addr);
   uint32_t index = addr >> 2;
-  uint32_t byte_off = addr & 0b11;
-  uint32_t bit_off = byte_off * 8;
+  uint32_t byte_off = addr & 0b11; // byte offset 字节有几个偏移
+  uint32_t bit_off = byte_off * 8; // 相应的 bit 有几个偏移的 bit
 
   // peripheral access
   size_t peripheral_id;
-  if (is_peripheral(addr, &peripheral_id)) {
+  if (is_peripheral(addr, &peripheral_id))
+  {
     peripheral_descr p = peripherals[peripheral_id];
     return p.callback_r(addr - p.base_addr, mode);
   }
@@ -22,23 +40,26 @@ uint32_t mem_load(uint32_t addr, AccessMode mode, uint32_t is_signed) {
   // memory access
   Assert(addr < MEM_SZ, "Memory access out of bound");
   uint32_t result;
-  switch (mode) {
+  switch (mode)
+  {
   case ACCESS_BYTE:
-    result = (memory[index] >> bit_off) & 0xFF;
-    if (is_signed) {
+    result = (memory[index] >> bit_off) & 0xFF; // 取一个 Byte
+    if (is_signed)
+    {
       result = (int32_t)((int8_t)result);
     }
     break;
   case ACCESS_HWORD:
     Assert(!(addr & 0b01), "Illegal access: Memory access unaligned.");
-    result = (memory[index] >> bit_off) & 0xFFFF;
-    if (is_signed) {
+    result = (memory[index] >> bit_off) & 0xFFFF; // 取一个 half word
+    if (is_signed)
+    {
       result = (int32_t)((int16_t)result);
     }
     break;
-  case ACCESS_WORD:
+  case ACCESS_WORD: // 你看, word 就不用 signed
     Assert(!(addr & 0b11), "Illegal access: Memory access unaligned.");
-    result = memory[index];
+    result = memory[index]; // 取一个 word
     break;
   default:
     panic("Bad byte access mode. Only Word/Half word/Byte is supported.");
@@ -47,29 +68,39 @@ uint32_t mem_load(uint32_t addr, AccessMode mode, uint32_t is_signed) {
   return result;
 }
 
-void mem_store(uint32_t addr, AccessMode mode, uint32_t value) {
+/**
+ * @brief store 以及 mmio
+ *
+ * @param addr
+ * @param mode
+ * @param value
+ */
+void mem_store(uint32_t addr, AccessMode mode, uint32_t value /* 这个与上面相比, 就没有 signed */)
+{
   uint32_t index = addr >> 2;
   uint32_t byte_off = addr & 0b11;
   uint32_t bit_off = byte_off * 8;
 
   // peripheral access
   size_t peripheral_id;
-  if (is_peripheral(addr, &peripheral_id)) {
+  if (is_peripheral(addr, &peripheral_id))
+  {
     peripheral_descr p = peripherals[peripheral_id];
     return p.callback_w(addr - p.base_addr, mode, value);
   }
 
   // memory access
   Assert(addr < MEM_SZ, "Memory access out of bound");
-  switch (mode) {
+
+  // 对于 sb 和 sh 来说, 他会做一个掩码的操作
+  switch (mode)
+  {
   case ACCESS_BYTE:
-    memory[index] =
-        memory[index] & (~(0xFF << bit_off)) | ((value & 0xFF) << bit_off);
+    memory[index] = memory[index] & (~(0xFF << bit_off)) | ((value & 0xFF) << bit_off);
     return;
   case ACCESS_HWORD:
     Assert(!(addr & 0b01), "Illegal access: Memory access unaligned.");
-    memory[index] =
-        memory[index] & (~(0xFFFF << bit_off)) | ((value & 0xFFFF) << bit_off);
+    memory[index] = memory[index] & (~(0xFFFF << bit_off)) | ((value & 0xFFFF) << bit_off);
     return;
   case ACCESS_WORD:
     Assert(!(addr & 0b11), "Illegal access: Memory access unaligned.");
@@ -80,7 +111,8 @@ void mem_store(uint32_t addr, AccessMode mode, uint32_t value) {
   }
 }
 
-MEM2WB MEM(EX2MEM ex_info) {
+MEM2WB MEM(EX2MEM ex_info)
+{
   MEM2WB ret;
   // passthrough
   ret.alu_out = ex_info.alu_out;
@@ -91,13 +123,16 @@ MEM2WB MEM(EX2MEM ex_info) {
   ret.target_pc = ex_info.target_pc;
   ret.inst = ex_info.inst;
   ret.pc = ex_info.pc;
+
   Log("PC = %8.8x", ret.pc);
   uint32_t load_result = 0;
   uint32_t is_store = 0;
   AccessMode mode = ACCESS_BYTE;
   uint32_t is_signed = 0;
-  if (ex_info.is_mem) {
-    switch (ex_info.mem_op) {
+  if (ex_info.is_mem)
+  {
+    switch (ex_info.mem_op)
+    {
     case MEM_SB:
       mode = ACCESS_BYTE;
       is_store = 1;
@@ -135,11 +170,17 @@ MEM2WB MEM(EX2MEM ex_info) {
       is_store = 0;
       is_signed = 0;
     }
-    if (is_store) {
+
+    // 如果是我, 我肯定不会这么写, 因为确实: 很不函数式
+    if (is_store)
+    {
       mem_store(ret.alu_out, mode, ex_info.store_val);
-    } else {
+    }
+    else
+    {
       ret.load_out = mem_load(ret.alu_out, mode, is_signed);
     }
   }
+
   return ret;
 }
--- golden_model/stage/WB.c
+++ golden_model/stage/WB.c
@@ -1,14 +1,22 @@
 #include <cpu.h>
+
 extern riscv32_CPU_state cpu;
-WB_info WB(MEM2WB mem_info) {
+
+WB_info WB(MEM2WB mem_info)
+{
   WB_info ret;
   ret.wb_have_inst = 1;
-  if (mem_info.branch_taken) {
+  if (mem_info.branch_taken)
+  {
+    // 访问全局状态
+    // 如果前面有发生 branch/jal/jalr 的话, npc 就会被更新, 否则默认是 npc = pc + 4
     cpu.npc = mem_info.target_pc;
   }
   uint32_t wb_val;
-  if (mem_info.wb_en) {
-    switch (mem_info.wb_sel) {
+  if (mem_info.wb_en)
+  {
+    switch (mem_info.wb_sel)
+    {
     case WB_ALU:
       wb_val = mem_info.alu_out;
       break;
@@ -24,18 +32,21 @@ WB_info WB(MEM2WB mem_info) {
     }
     cpu.gpr[mem_info.dst] = wb_val;
   }
-  ret.wb_value = wb_val;
+
+  ret.wb_value = wb_val; // 确实没有完全初始化, 但是好像没啥关系
   ret.wb_ena = mem_info.wb_en;
   ret.wb_pc = mem_info.pc;
   ret.wb_reg = mem_info.dst;
   cpu.gpr[0] = 0;
   Log("WB Stage:");
   Log("PC = %8.8x", mem_info.pc);
-  if (mem_info.wb_en) {
+  if (mem_info.wb_en)
+  {
     Log("WB value = %8.8x, WReg = %d, npc = 0x%8.8x", wb_val, mem_info.dst,
         cpu.npc);
   }
-  if (mem_info.branch_taken) {
+  if (mem_info.branch_taken)
+  {
     Log("Branch Taken, target is %8.8x", mem_info.target_pc);
   }
   return ret;
